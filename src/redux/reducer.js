97ed5c963f8bb32edfb77a422974d03af983f5e7
fearm3 <ibraaa1628@gmail.com>
2022-02-18 01:15:20 +0300

    add bug fixed

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -24,7 +24,7 @@ export const rootReducer = (state = initialState, action) => {
         return { lastViews: [...newState] };
       }
     case GET_VIEWS:
-      return state;
+      return { lastViews: [...action.payload] };
 
     default: {
       return state;
